2d5fdb870c361b90b3e18a36ef611a0c3ead9581
Baruch Lavy <ff>
2025-11-10 11:07:57 +0200

    add user

---
+++ user.py
@@ -0,0 +1,21 @@
+class Book:
+    pass
+
+
+class Member:
+    def __init__(self,id,full_name,active_loans_count=0):
+        self.id = id
+        self.full_name = full_name
+        self.loans = active_loans_count
+        self.borrowed_books:list[Book] = []
+    
+    def borrow_book(book:Book)
+       
+    def increment_loans(self):
+        self.loans += 1
+        
+    def decrement_loans(self):
+        if (self.loans - 1) < 0:
+            self.loans = 0
+        else:   
+            self.loans -= 1
